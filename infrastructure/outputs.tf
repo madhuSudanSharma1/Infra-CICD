27ca2f4b9c292de1c7481e4d339f8424b18734a4
madhu <077bct042.madhu@pcampus.edu.np>
2025-07-12 16:27:26 +0545

    updated

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,4 +1,4 @@
-output "ec2_role_arn" {
-  value = aws_iam_role.ec2_s3_role.arn
-  description = "The ARN of the IAM role attached to the EC2 instance"
+output "aws_instance_id" {
+  value = aws_instance.ec2-instance.id
+  
 }
